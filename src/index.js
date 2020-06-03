677202c90f65c48a2b6398593ecf2742235b0ad3
Leo Gharib <leonargharib@gmail.com>
2020-06-03 13:14:17 -0700

    set account address in state

--- src/index.js
+++ src/index.js
@@ -2,45 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import MusicControls from "./MusicControls.js";
-import axios from 'axios';
+import axios from "axios";
 
 let ethereum = window.ethereum;
 var userAddress;
-
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        account: null
-    }
+      accountAddress: "",
+    };
   }
   componentDidMount() {
     this.getAccountName();
-}
-
-async getAccountName() {
-    // eslint-disable-next-line no-undef
-    this.setState({account: await ethereum.enable()}, () => {
-        userAddress = this.state.account[0];
-        console.log("userAddress: "+userAddress);
-    });
-}
-
-
-  
-  render() {
-      return (
-        <div className="container">
-          <h1>Test</h1>
-          <MusicControls  walletAddress={123}/>
-        </div>
-    );
-    
-    }
-    
-
-
   }
+
+  async getAccountName() {
+    // eslint-disable-next-line no-undef
+    const accountInfo = await ethereum.enable();
+    userAddress = accountInfo[0];
+    console.log("userAddress: " + userAddress);
+    this.setState({ accountAddress: userAddress });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Test</h1>
+        <MusicControls walletAddress={this.state.accountAddress} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
